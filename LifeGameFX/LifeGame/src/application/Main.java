72c83018e871e8430d389ecf875d419562e702e0
CrowleyXavier <janjanjanjan5738@ezweb.ne.jp>
2019-06-05 13:49:07 +0900

    First commit
    This is first commit.

---
+++ LifeGameFX/LifeGame/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Form.fxml"));
+			Scene scene = new Scene(root,500,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
